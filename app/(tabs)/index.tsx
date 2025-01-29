4408b59a9b3dd498399824ffa772aae881ba2cce
Sarmad Rafique <sarmadrafique040@gmail.com>
2025-01-29 09:16:07 +0500

    Simplified the home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,36 +1,11 @@
-import { Image, StyleSheet, Platform, SafeAreaView, Text } from "react-native";
+import { SafeAreaView, Text } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
-      <Text className="text-white bg-indigo-500 p-3 text-2xl">
-        Hi, this is Sarmad
+    <SafeAreaView className="flex-1 items-center justify-center">
+      <Text className="text-white bg-indigo-500 p-3 text-2xl rounded-md font-bold">
+        Hi, this is Sarmad Rafique
       </Text>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
-  },
-  textStyles: {
-    color: "red",
-    backgroundColor: "yellow",
-    borderRadius: 8,
-    padding: 10,
-  },
-});
